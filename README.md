1205337a8dcf238a9d2d212b376cc7ab6b7b6821
Pierre Zemb <contact@pierrezemb.fr>
2020-03-08 20:16:17 +0100

    adding cg and cheatsheet

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # containers-vs-vms-demo
 All the demo for the `Containers vs VMs` lecture
+
+## Cheatsheets
+
+```bash
+
+# download gentoo stage
+./init.sh
+
+# become root
+
+# mount things
+mount --types proc /proc ./mychroot/proc
+mount --rbind /sys ./mychroot/sys
+mount --make-rslave ./mychroot/sys
+mount --rbind /dev ./mychroot/dev
+mount --make-rslave ./mychroot/dev
+
+# chroot
+chroot ./mychroot /bin/bash
+
+# unmount things
+umount -l ./mychroot/{proc,sys}
+umount -l ./mychroot/sys
+umount -l ./mychroot/dev
+
+# systemd-nspawn
+systemd-nspawn -D mychroot
+
+# dockerlite
+cd dockerlite
+./dockerlite run /bin/bash
+```
--- dockerlite/main.go
+++ dockerlite/main.go
@@ -1,11 +1,14 @@
 package main
 
-// https://raw.githubusercontent.com/lizrice/containers-from-scratch/master/main.go
+// https://raw.githubusercontent.com/pierrerice/containers-from-scratch/master/main.go
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
@@ -23,6 +26,8 @@ func main() {
 
 func run() {
 	fmt.Printf("Running %v \n", os.Args[2:])
+
+	cg()
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
@@ -74,4 +79,14 @@ func must(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func cg() {
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	os.Mkdir(filepath.Join(pids, "pierre"), 0755)
+	must(ioutil.WriteFile(filepath.Join(pids, "pierre/pids.max"), []byte("20"), 0700))
+	// Removes the new cgroup in place after the container exits
+	must(ioutil.WriteFile(filepath.Join(pids, "pierre/notify_on_release"), []byte("1"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "pierre/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
